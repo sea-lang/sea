6fd5eecd7536a12414a63844e2c596ec80e50cad
EmmaTheMartian <emmathemartian@gmail.com>
2025-04-21 07:02:37 -0500

    Working on a cli (transferring machines)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{fs, path::PathBuf, str::FromStr};
 
+use clap;
+
 use parse::{lexer::make_lexer, parser::Parser, polish_notation::PolishNodeTree};
 use sandbox::Sandbox;
 
@@ -8,20 +10,97 @@ pub mod hashtags;
 pub mod parse;
 pub mod sandbox;
 
-fn main() {
-    // let mut sandbox = Sandbox::new();
-    // sandbox.start();
+#[derive(Debug, clap::Parser)]
+#[command(name = "Sea")]
+#[command(version = "0.0.1-dev")]
+#[command(about = "Sea compiler", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    let path = PathBuf::from_str("test.sea").unwrap();
+#[derive(clap::Subcommand)]
+enum Commands {
+    Compile(CompileArgs),
+    Run(CompileArgs, RunArgs),
+    Sandbox(SandboxArgs),
+}
+
+#[derive(clap::Args)]
+struct CompileArgs {
+    /// The file to compile
+    file: Option<PathBuf>,
+    /// The path to the output file
+    #[arg(short, long, value_name = "FILE", default_value_t = "main")]
+    output: PathBuf,
+    /// Toggles optimizations for production builds
+    #[arg(short, long)]
+    prod: bool,
+    /// The C compiler to build with
+    #[arg(short, long, value_name = "COMPILER")]
+    cc: Option<String>,
+    /// Arguments for the C compiler
+    #[arg(short = 'f', long, value_name = "COMPILER FLAGS")]
+    ccflags: Option<String>,
+    /// Paths to search for libraries
+    #[arg(short, long)]
+    libpaths: Option<String>,
+    /// Path to the standard library
+    #[arg(short, long)]
+    std: String,
+    /// Disables implicit `use std`
+    #[arg(short = 'S', long)]
+    nostd: bool,
+}
+
+#[derive(clap::Args)]
+struct RunArgs {
+    /// Disables rebuild
+    #[arg(short, long)]
+    nobuild: bool,
+    // Args to pass to the program
+    #[arg(short, long, value_name = "PROGRAM ARGS", default_value_t = "")]
+    args: String,
+}
+
+#[derive(clap::Args)]
+struct SandboxArgs {}
+
+fn compile(args: CompileArgs) {
+    let path = PathBuf::from_str(args.file).unwrap();
     let code = fs::read_to_string(&path).unwrap();
+
+    let mut parser = Parser::make_parser(make_lexer(path, &code));
+    parser.parse().pretty_print();
+}
+
+fn run(args: RunArgs) {
+    unimplemented!()
+}
+
+fn sandbox(args: SandboxArgs) {
+    let mut sandbox = Sandbox::new();
+    sandbox.start();
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Compile(args) => compile(args),
+        Commands::Run(compile_args, run_args) => {
+            if !run_args.nobuild {
+                compile(compile_args);
+            }
+            run(run_args);
+        }
+        Commands::Sandbox(args) => sandbox(args),
+    }
 
     // let mut lexer = make_lexer(PathBuf::new(), &code);
     // while let Some(tok) = lexer.next_token() {
     //     println!("tok: {:?}", tok);
     // }
-
-    let mut parser = Parser::make_parser(make_lexer(path, &code));
-    parser.parse().pretty_print();
 
     // let expr = "thing(1 + 2, -3 + 4)^.2".to_string();
     // println!("Expression: {expr}");

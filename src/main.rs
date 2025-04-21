9d638c76fe881a333920e8ab5c05ed7572cba36d
Emmeline <emmathemartian@gmail.com>
2025-04-21 15:01:03 +0000

    Yippie CLI

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,61 @@
-use std::{fs, path::PathBuf, str::FromStr};
+use std::fs;
 
-use clap;
-
-use parse::{lexer::make_lexer, parser::Parser, polish_notation::PolishNodeTree};
-use sandbox::Sandbox;
+use parse::{lexer::make_lexer, parser::Parser};
 
 pub mod error;
 pub mod hashtags;
 pub mod parse;
 pub mod sandbox;
 
-#[derive(Debug, clap::Parser)]
-#[command(name = "Sea")]
-#[command(version = "0.0.1-dev")]
-#[command(about = "Sea compiler", long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
+mod flags {
+    use std::path::PathBuf;
+
+    xflags::xflags!(
+        cmd app {
+            cmd compile c {
+                /// The file to compile
+                required input: PathBuf
+                /// The path to the output file
+                optional -o, --output output: PathBuf
+                /// Executes the program after compilation
+                optional -r, --run
+                /// Toggles optimizations for production builds
+                optional -p, --prod
+                /// The C compiler to build with
+                optional -c, --cc cc: String
+                /// Arguments for the C compiler
+                optional -f, --ccflags ccflags: String
+                /// Paths to search for libraries
+                optional -l, --libpaths libpaths: String
+                /// Path to the standard library
+                optional -s, --std std: String
+                /// Disables implicit `use std`
+                optional -S, --nostd
+            }
+            cmd sandbox s {
+            }
+        }
+    );
 }
 
-#[derive(clap::Subcommand)]
-enum Commands {
-    Compile(CompileArgs),
-    Run(CompileArgs, RunArgs),
-    Sandbox(SandboxArgs),
-}
+fn compile(flags: flags::Compile) {
+    let code = fs::read_to_string(flags.input.clone()).unwrap();
 
-#[derive(clap::Args)]
-struct CompileArgs {
-    /// The file to compile
-    file: Option<PathBuf>,
-    /// The path to the output file
-    #[arg(short, long, value_name = "FILE", default_value_t = "main")]
-    output: PathBuf,
-    /// Toggles optimizations for production builds
-    #[arg(short, long)]
-    prod: bool,
-    /// The C compiler to build with
-    #[arg(short, long, value_name = "COMPILER")]
-    cc: Option<String>,
-    /// Arguments for the C compiler
-    #[arg(short = 'f', long, value_name = "COMPILER FLAGS")]
-    ccflags: Option<String>,
-    /// Paths to search for libraries
-    #[arg(short, long)]
-    libpaths: Option<String>,
-    /// Path to the standard library
-    #[arg(short, long)]
-    std: String,
-    /// Disables implicit `use std`
-    #[arg(short = 'S', long)]
-    nostd: bool,
-}
-
-#[derive(clap::Args)]
-struct RunArgs {
-    /// Disables rebuild
-    #[arg(short, long)]
-    nobuild: bool,
-    // Args to pass to the program
-    #[arg(short, long, value_name = "PROGRAM ARGS", default_value_t = "")]
-    args: String,
-}
-
-#[derive(clap::Args)]
-struct SandboxArgs {}
-
-fn compile(args: CompileArgs) {
-    let path = PathBuf::from_str(args.file).unwrap();
-    let code = fs::read_to_string(&path).unwrap();
-
-    let mut parser = Parser::make_parser(make_lexer(path, &code));
+    let mut parser = Parser::make_parser(make_lexer(flags.input.clone(), &code));
     parser.parse().pretty_print();
 }
 
-fn run(args: RunArgs) {
-    unimplemented!()
-}
-
-fn sandbox(args: SandboxArgs) {
-    let mut sandbox = Sandbox::new();
+fn sandbox(_flags: flags::Sandbox) {
+    let mut sandbox = sandbox::Sandbox::new();
     sandbox.start();
 }
 
 fn main() {
-    let cli = Cli::parse();
+    let flags = flags::App::from_env_or_exit();
 
-    match &cli.command {
-        Commands::Compile(args) => compile(args),
-        Commands::Run(compile_args, run_args) => {
-            if !run_args.nobuild {
-                compile(compile_args);
-            }
-            run(run_args);
-        }
-        Commands::Sandbox(args) => sandbox(args),
+    match flags.subcommand {
+        flags::AppCmd::Compile(args) => compile(args),
+        flags::AppCmd::Sandbox(args) => sandbox(args),
     }
 
     // let mut lexer = make_lexer(PathBuf::new(), &code);
